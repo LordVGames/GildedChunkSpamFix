fa3ad45157545b4a53ca4c76cba7db9bf6f9add8
LordVGames <lordvgames810@gmail.com>
2025-01-26 20:43:38 -0500

    made IL hook simpler and better
    imagine removing IL haha couldn't be me

--- GildedChunkSpamFix/Main.cs
+++ GildedChunkSpamFix/Main.cs
@@ -13,7 +13,7 @@ namespace GildedChunkSpamFix
         public const string PluginGUID = PluginAuthor + "." + PluginName;
         public const string PluginAuthor = "LordVGames";
         public const string PluginName = "GildedChunkSpamFix";
-        public const string PluginVersion = "1.0.0";
+        public const string PluginVersion = "1.0.1";
 
         public void Awake()
         {
@@ -27,43 +27,47 @@ namespace GildedChunkSpamFix
         public static void AffixAurelioniteBehavior_OnServerDamageDealt(ILContext il)
         {
             ILCursor c = new(il);
-            // replace "this.isPlayer" check at 241 (or 112 in IL) with "!DamageReport.victimBody.isPlayerControlled"
-            // that way both player and AI attackers will have a cooldown for spawning gilded chunks, not just players
-            // gold chunk spam can still happen on player victims, but im 99.999% sure that'll never be a problem in normal gameplay
-            if (c.TryGotoNext(MoveType.Before,
-                    x => x.MatchLdloc(3),
-                    x => x.MatchLdcR4(out _),
-                    x => x.MatchBleUn(out _)
-                ))
-            {
-                ILLabel addressJustStealMoney = c.DefineLabel();
-                c.MarkLabel(addressJustStealMoney);
 
-                // resetting the cursor position because the label was further than where we need to emit code
-                c.Index = 0;
-                c.GotoNext(MoveType.AfterLabel,
-                    x => x.MatchLdarg(0),
-                    x => x.MatchLdfld<AffixAurelioniteBehavior>("isPlayer"),
-                    x => x.MatchBrfalse(out _)
-                );
-                c.RemoveRange(3);
-                c.Emit(OpCodes.Ldarg_1);
-                c.Emit<DamageReport>(OpCodes.Ldfld, "victimBody");
-                c.Emit<CharacterBody>(OpCodes.Callvirt, "get_isPlayerControlled");
-                c.Emit(OpCodes.Brtrue_S, addressJustStealMoney);
+
+            if (!c.TryGotoNext(MoveType.After,
+                x => x.MatchLdarg(0),
+                x => x.MatchLdfld<AffixAurelioniteBehavior>("isPlayer"),
+                x => x.MatchBrfalse(out _)
+            ))
+            {
+                LogILError(il, c);
+            }
+            // create label here to jump past the isPlayer check later
+            ILLabel doCooldownStuff = c.DefineLabel();
+            c.MarkLabel(doCooldownStuff);
+
+
+            // resetting cursor position & going before the isPlayer check using AfterLabel to not mess anything up (or at least it shouldn't)
+            c.Index = 0;
+            if (!c.TryGotoNext(MoveType.AfterLabel,
+                x => x.MatchLdarg(0),
+                x => x.MatchLdfld<AffixAurelioniteBehavior>("isPlayer"),
+                x => x.MatchBrfalse(out _)
+            ))
+            {
+                LogILError(il, c);
+            }
+            // make it always jump to the stuff for cooldowns for spawning gold, regardless of who is attacker/victim
+            c.Emit(OpCodes.Br, doCooldownStuff);
+
 
 #if DEBUG
-                Log.Warning("SUCCESSFULLY IL HOOKED AffixAurelioniteBehavior OnServerDamageDealt");
-                Log.Warning($"cursor is {c}");
-                Log.Warning($"il is {il}");
+            Log.Warning("SUCCESSFULLY IL HOOKED AffixAurelioniteBehavior OnServerDamageDealt");
+            Log.Warning($"cursor is {c}");
+            Log.Warning($"il is {il}");
 #endif
-            }
-            else
-            {
-                Log.Error("COULD NOT IL HOOK AffixAurelioniteBehavior OnServerDamageDealt");
-                Log.Error($"cursor is {c}");
-                Log.Error($"il is {il}");
-            }
+        }
+
+        private static void LogILError(ILContext il, ILCursor c)
+        {
+            Log.Error("COULD NOT IL HOOK AffixAurelioniteBehavior OnServerDamageDealt");
+            Log.Error($"cursor is {c}");
+            Log.Error($"il is {il}");
         }
     }
 }
--- Thunderstore/Package Files/README.md
+++ Thunderstore/Package Files/README.md
@@ -2,9 +2,7 @@
 
 As the description says, this fixes gilded drones, friendly monsters, or any AI that is a gilded elite spawning so many gold chunks the game lags out and practically crashes.
 
-It does this by making both players AND drones/monsters/any AI use a cooldown for how often gold chunks can spawn. Only players have this normally, which leads to monsters not having a cooldown and crashing games with gold.
-
-<sub><sup>it's still possible to have gold spam from players getting hit but i'm very sure that'll never happen in normal gameplay</sup></sub>
+It does this by making both players AND drones/monsters/any AI use a cooldown for how often gold chunks can spawn. Only players have this normally, which leads to AI not having a cooldown and crashing games with gold.
 
 ## Before
 ![Risk_of_Rain_2_yaRxN4H3L8](https://github.com/user-attachments/assets/112db86d-f2ac-4d70-9aa6-1cacedf47545)
